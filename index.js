b2c5d2b5626333f5154815808856a9cc911bb99c
Aryan <aryan@pop-os.localdomain>
2021-09-28 19:55:26 +0530

    prescription format done

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const pdf = require("pdf-creator-node");
 const fs = require("fs");
 const html = fs.readFileSync("./template/template.html", "utf-8");
+const data = require("./data.json");
 
 const document = {
   html: html,
   path: "./output.pdf",
   type: "",
-  data: {},
+  data: data,
 };
 
 var options = {
   format: "A4",
   orientation: "portrait",
-  border: "10mm",
+  border: "8mm",
 };
 
 pdf
